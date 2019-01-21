4bda150bf345ab2d74987cdf91da6be728f9fb29
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-21 10:33:52 +0200

    pirma uzduotis

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set("Europe/Vilnius");
+$kelintadienis = date('d');
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title><?php print $kelintadienis ?></title>
+    </head>
+    <body>
+        <h1>Tadas- HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date('y') . ' metu' ?> </h1>
+        <p>Viskas prasidejo <?php print date("m") . ' Menesio ' . date("d") . ' diena' ?></p>
+    </body>
+</html>

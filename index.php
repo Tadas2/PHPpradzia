ff5eb5b6c1b2135d8fc0aaa67542831761d0a26a
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-21 11:20:38 +0200

    lol

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>PHP lydes ir <?php print date('d', strtotime('+1 day')) ?></title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1>PHP su manim buvo ir <?php print date('H', strtotime('-1 hour')) . 'valanda' ?> </h1>
-        <p><?php print date('y', strtotime('+1 year')) . ' ne uz kalnu' ?></p>
+        <div>
+            <h1>as pradedu</h1>
+            <h2>o as papildau</h2>
+        </div>
+        <div>
+            <p>o as esu alus</p>
+        </div>
     </body>
 </html>

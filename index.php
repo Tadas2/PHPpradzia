ebd2790cdae9445f885002554589e89f9cc4257e
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-21 13:17:37 +0200

    sekundes

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Komentarai</title>
-    </head>
-    <body>
-        <!-- aiskinu savo diva -->
-
-        <?php /*
-          <div>
-          <h1>as pradedu</h1>
-          <h2>o as papildau</h2>
-          </div>
-         */ ?>
-
-        <!-- iskomentinu diva
-        <div>
-            <p>o as esu alus</p>
-        </div>
-        -->
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <title>babushka</title>
+    <link rel="stylesheet" href="css/style.css"> 
+  </head>
+  <body>
+      <div><?php print date('s') ?></div>
+  </body>
 </html>

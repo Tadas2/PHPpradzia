a8f720aa01e395ddf4944c145382de42e5c796c3
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-22 11:50:46 +0200

    asds

--- index.php
+++ index.php
@@ -4,17 +4,16 @@ $antr_pask = rand(101, 200);
 $trec_pask = rand(201, 300);
 ?>
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <title>babushka</title>      
-    </head>
-    <body>
-        <div>
-            <h1>Skolos skaiciuokle</h1>
-            <p>jei paemei <?php print $pirm_pask ?>jievru<br>
-                su dviem kabanciais grazinsi <?php print $trec_pask ?><br>
-                su vienu kabanciu grazinsi <?php $antr_pask ?>
-            </p>
-        </div>
-    </body>
+<head>
+    <title>babushka</title>      
+</head>
+<body>
+    <div>
+        <h1>Skolos skaiciuokle</h1>
+        <p>jei paemei <?php print $pirm_pask ?>jievru<br>
+           su dviem kabanciais grazinsi <?php print $trec_pask ?><br>
+           su vienu kabanciu grazinsi <?php $antr_pask ?>
+        </p>
+    </div>
+</body>
 </html>

005186c56e32e07c152eaf580da4b9428557312e
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-22 11:50:04 +0200

    pataisymas

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ $trec_pask = rand(201, 300);
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-    <link: rel="stylesheet" href="css/style.css"
-           <meta charset="utf-8">
         <title>babushka</title>      
     </head>
     <body>

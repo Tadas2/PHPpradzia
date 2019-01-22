7037f433d85ef6d53bd282cfe9f01f0d414e6e8c
Tadas2 <tadas.zilinskas74@gmail.com>
2019-01-22 11:47:18 +0200

    paskolos skaiciuokle

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
+<?php
+$pirm_pask = rand(1, 100);
+$antr_pask = rand(101, 200);
+$trec_pask = rand(201, 300);
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>babushka</title>
-    <link rel="stylesheet" href="css/style.css"> 
-  </head>
-  <body>
-      <div><?php print date('s') ?></div>
-  </body>
+    <head>
+    <link: rel="stylesheet" href="css/style.css"
+           <meta charset="utf-8">
+        <title>babushka</title>      
+    </head>
+    <body>
+        <div>
+            <h1>Skolos skaiciuokle</h1>
+            <p>jei paemei <?php print $pirm_pask ?>jievru<br>
+                su dviem kabanciais grazinsi <?php print $trec_pask ?><br>
+                su vienu kabanciu grazinsi <?php $antr_pask ?>
+            </p>
+        </div>
+    </body>
 </html>
